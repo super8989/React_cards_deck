e2102a07923ef8f2bc972220debba6a6c2b11358
Sam <super8989@gmail.com>
2019-11-01 22:54:27 -0400

    installed axios to fetch deck from deckofcardsapi

--- src/Deck.js
+++ src/Deck.js
@@ -1,9 +1,19 @@
 import React, { Component } from "react";
+import axios from "axios";
+
+const API_URL = "https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1";
 
 class Deck extends Component {
 	constructor(props) {
 		super(props);
 		this.state = { deck: null };
+	}
+
+	async componentDidMount() {
+		let deck = await axios.get(API_URL);
+		this.setState({ deck: deck.data });
+
+		console.log(deck.data);
 	}
 
 	render() {
